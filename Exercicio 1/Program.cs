3f55d5fde7fd6f818836c09dfd8d9d4de1396ebb
merbach020202 <eduardo.brenn2006@gmail.com>
2023-03-31 10:55:32 -0300

    parei no exercicio 3

---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+Console.WriteLine($"Em qual dia você nasceu?: ");
+int dia = int.Parse(Console.ReadLine());
+//DIAS
+if (dia > 31 )
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Valor inválido (O dia foi digitado incorretamente)");
+    Console.ResetColor();
+}
+
+else 
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Valor válido");
+    Console.ResetColor();
+}
+
+//MESES
+Console.WriteLine($"Em qual mês você nasceu?: ");
+int meses = int.Parse(Console.ReadLine());
+
+if (meses > 12)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Valor inválido (O mês foi digitado incorretamente)");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Valor válido");
+    Console.ResetColor();
+}
+
+//ANO
+Console.WriteLine($"Qual o ano em que você nasceu: ");
+int anos = int.Parse(Console.ReadLine());
+
+if (anos <= 2013 && anos >2023)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Valor inválido (O ano digitado esta incorreto)");
+    Console.ResetColor();
+}
+
+else{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Valor válido");
+    Console.ResetColor();
+}
+
+
+
+
+
+
+
+
+
+ 
---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,55 @@
+﻿
+Console.WriteLine("Digite o 1º número");
+int número = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o 2º número");
+int número2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o 3º número");
+int número3 = int.Parse(Console.ReadLine());
+
+if (número > número2 && número > número3)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"O maior número é {número}");
+    Console.ResetColor();
+}
+else if (número2 > número && número2 > número3)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"O maior número é {número2}");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"O maior número é {número3}");
+    Console.ResetColor();
+}
+
+
+if (número < número2 && número < número3)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"O menor número é {número}");
+    Console.ResetColor();
+}
+else if (número2 < número && número2 < número3)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"O menor número é {número2}");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"O menor número é {número3}");
+    Console.ResetColor();
+}
+
+
+
+
+
+
+
